adaf7c85a7830dbfe097db9d6bdc183e26f6e013
afroza1717 <55728909+afroza1717@users.noreply.github.com>
2025-02-17 15:29:47 -0500

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,21 @@
+function fetchWeather(city) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Weather in ${city} is 75\u00B0F`);
+        }, 2000);
+    });
+}
+
+fetchWeather('New York').then(messageWeather => {
+    console.log(messageWeather);
+    return fetchWeather("Los Angeles");
+}).then(messageWeather => {
+    console.log(messageWeather);
+    return fetchWeather("Chicago")
+}).then(messageWeather => {
+    console.log(messageWeather);
+    console.log("Weather updates completed!");
+})
+
+
+
